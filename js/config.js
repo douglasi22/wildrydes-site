b456a33d27397014c00dddfa5b9654a28fcc3c75
douglasi22 <direland@sofi.org>
2020-09-24 15:31:52 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_FArJOXJ4I', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5cs99sqdtb2uhj84kj7e0l1r50', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
